413ec8589aec46aa78f537d88f91368edbc4e97f
gus <ggortiz09@gmail.com>
2021-03-12 13:25:31 -0300

    front end init

---
+++ src/main/java/com/go/sg/ProyectSgApplication.java
@@ -0,0 +1,13 @@
+package com.go.sg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectSgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectSgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/go/sg/ProyectSgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.go.sg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectSgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
